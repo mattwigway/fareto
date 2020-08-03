98ea2e19d80daa618193fbdd793f5c16c64b408b
Matthew Wigginton Conway <matt@indicatrix.org>
2020-08-03 16:18:24 -0400

    show seconds

--- src/timeutil.js
+++ src/timeutil.js
@@ -2,7 +2,8 @@
 export function secondsToTime (seconds) {
     const hours = Math.floor(seconds / 3600)
     const minutes = Math.floor((seconds % 3600) / 60)
-    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`
+    const secs = seconds % 60
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`
 }
 
 /** HH:MM to seconds since midnight */
