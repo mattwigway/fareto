c19640da34fa988016d4b82307e08f2f303a457f
Matthew Wigginton Conway <matt@indicatrix.org>
2020-08-03 16:18:35 -0400

    regex corrections

--- src/ProfileRequestUI.js
+++ src/ProfileRequestUI.js
@@ -52,7 +52,7 @@ export default class ProfileRequestUI extends React.Component {
                 // for safety, make sure there are no slashes, etc. in the url
                 // in case this is ever deployed on an authenticated site, someone might be able to do something
                 // nasty by forcing requests to their domain with a malicious URL (I don't think they could, but maybe)
-                if (/^[a-zA-Z0-9-_]+$/.test(load)) {
+                if (/^[a-zA-Z0-9\-_]+$/.test(load)) {
                     fetch(`results/${load}.json`)
                     .then(async (res) => {
                         if (res.ok) {
@@ -86,7 +86,7 @@ export default class ProfileRequestUI extends React.Component {
             // enforce formatting of strings
             if (!/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/.test(hashState.date)) {
                 this.props.setError('invalid date format, should be YYYY-MM-DD')
-            } else if (!/^[a-zA-Z]+$/.test(hashState.fareCalculatorType)) {
+            } else if (!/^[a-zA-Z0-9\-]+$/.test(hashState.fareCalculatorType)) {
                 this.props.setError('invalid fare calculator type') 
             } else {
                 // Make a new request
